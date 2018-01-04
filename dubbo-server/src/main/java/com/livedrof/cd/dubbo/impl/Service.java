ac1f382157290cdb44a1204e9e88680592fae2e8
felit <nofelit@126.com>
2018-01-05 01:08:07 +0800

    完成dubbo的Service

--- dubbo-server/src/main/java/com/livedrof/cd/dubbo/impl/Service.java
+++ dubbo-server/src/main/java/com/livedrof/cd/dubbo/impl/Service.java
@@ -4,6 +4,7 @@ import com.livedrof.cd.dubbo.IService;
 
 public class Service implements IService {
     public String sayHello(String args) {
+        System.out.println("log from " + args);
         return "hello dubbo";
     }
 }
